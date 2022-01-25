529b483852ae1662e60773583a46988dcca4cec6
Demian Abiusi <demian@abiusi.com.ar>
2022-01-25 20:46:13 -0300

    complete code

--- index.php
+++ index.php
@@ -1,15 +1,37 @@
-<!DOCTYPE html>
+<?php
+
+/* Index file which displays a search box */
+
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   <?php require 'htmlhead.php'; ?>
 </head>
 <body>
     
+    <?php require 'header.php'; ?>
+   
+    <main>
+        <div class="container text-center">
+                <h3 class="py-3">Welcome</h3>
+
+                <form method="get" action="searchuser.php">
+                <div class="row">
+                    <div class="col-md-10">
+                        <input class="form-control" name="searchuser" placeholder="Please Enter a Valid Torre Username" >
+                    </div>
+                    <div class="col-md-2">
+                        <button class="btn btn-primary">Display</button>
+                    </div>
+                </div>
+                </form>
+
+        </div>
+    </main>
+
+
+    <script>
+    </script>
 </body>
 </html>
